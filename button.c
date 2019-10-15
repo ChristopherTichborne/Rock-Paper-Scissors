54c4b47637dfa61a25e09bd04a533ae34815b32b
Nicholas Tice Page <ntp23@cs18207kq.canterbury.ac.nz>
2019-10-16 12:58:24 +1300

    Button module for game

---
+++ button.c
@@ -0,0 +1,24 @@
+
+#ifndef BUTTON_H
+#define BUTTON_H
+
+
+#include <avr/io.h>
+#include "button.h"
+
+#define BIT(x) (1 << (x))
+
+/** Return non-zero if button pressed.  */
+int button_pressed_p (void)
+{
+    return (PIND & BIT(7));
+}
+
+
+/** Initialise button1.  */
+void button_init (void)
+{
+    DDRD |= (0 << 7);
+}
+
+#endif

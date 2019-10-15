7ad0bf88283522ddd1b1dfa1826ffb72ec907456
Christopher John Tichborne <cjt120@cs18258kq.canterbury.ac.nz>
2019-10-15 21:19:27 +1300

    Placed module inside function

--- character_choice.c
+++ character_choice.c
@@ -5,22 +5,23 @@
 
 #include "system.h"
 #include "navswitch.h"
-
-char choice = '_';
-
-if (navswitch_push_event_p(NAVSWITCH_EAST))    // East/Right for rock selection
+char character_choice(char choice)
 {
-    choice = 'R';
-}
 
-if (navswitch_push_event_p(NAVSWITCH_North))   // North/Up for paper selection
-{
-    choice = 'P';
-}
+    if (navswitch_push_event_p(NAVSWITCH_EAST))    // East/Right for rock selection
+    {
+        choice = 'R';
+    }
 
-if (navswitch_push_event_p(NAVSWITCH_West))    // West/Left for scissor selection
-{
-    choice = 'S';
-}
+    if (navswitch_push_event_p(NAVSWITCH_North))   // North/Up for paper selection
+    {
+        choice = 'P';
+    }
 
-return choice;
+    if (navswitch_push_event_p(NAVSWITCH_West))    // West/Left for scissor selection
+    {
+        choice = 'S';
+    }
+
+    return choice;
+}

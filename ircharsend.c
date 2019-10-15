a8c0d936a4f0b2c8647a8995921454b2acb52454
Christopher John Tichborne <cjt120@cs18258kq.canterbury.ac.nz>
2019-10-15 21:22:19 +1300

    Added module into function block

--- ircharsend.c
+++ ircharsend.c
@@ -5,8 +5,12 @@
 #include "system.h"
 #include "ir_uart.h"
 
-if (((choice == 'P' || choice == 'S' || choice == 'R') && ir_uart_write_ready_p()
-            && navswitch_push_event_p(NAVSWITCH_PUSH)))
-        {
-            ir_uart_putc(character);
-        }
+int send_character(char choice)
+{
+    if (((choice == 'P' || choice == 'S' || choice == 'R') && ir_uart_write_ready_p()
+            && navswitch_push_event_p(NAVSWITCH_PUSH))) {
+        ir_uart_putc(character);
+        return 1;
+    }
+    return 0;
+}

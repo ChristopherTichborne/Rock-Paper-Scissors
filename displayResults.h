156762b261f88344cecf342b686fa86c27a91a61
Nicholas Tice Page <ntp23@cs18207kq.canterbury.ac.nz>
2019-10-16 12:11:51 +1300

    Headrer for displayResults.c

---
+++ displayResults.h
@@ -0,0 +1,15 @@
+/** Header file for displayResults.h
+ * This header defines two functions used for displaying results
+*/
+
+#ifndef DISPLAYRESULTS_H
+#define DISPLAYRESULTS_H
+
+#include "system.h"
+
+void display_local(char localChar);
+
+void display_win_status(int gameStatus);
+
+#endif
+

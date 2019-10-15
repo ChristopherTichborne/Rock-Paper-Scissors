648d4eab250cba617d9894b42f05f4b9ae9dc48a
Christopher John Tichborne <cjt120@cs18206kq.canterbury.ac.nz>
2019-10-16 12:22:37 +1300

    Header file for winner selection

---
+++ check_winner.h
@@ -0,0 +1,7 @@
+#ifndef CHECK_WINNER_H
+#define CHECK_WINNER_H
+
+/* Sets game status flag to win, loss, or draw and returns as int */
+int check_winner(char localCharacter, char rivalCharacter);
+
+#endif

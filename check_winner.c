ce257dc0ecf55e93c2995e0659d1a3ea32324e67
Christopher John Tichborne <cjt120@cs18258kq.canterbury.ac.nz>
2019-10-15 21:59:52 +1300

    Module to check winner

---
+++ check_winner.c
@@ -0,0 +1,33 @@
+/*
+    Module to check winner status
+*/
+
+#include "system.h"
+
+int check_winner(char localCharacter, char rivalCharacter)
+{
+
+    if(localCharacter != '_' || rivalCharacter != '_') {
+        if(localCharacter == 'R' && rivalCharacter == 'S' ) {      // local selected rock and rival selected scissor
+            gameStatus = 1;
+        }
+
+        else if(localCharacter == 'P' && rivalCharacter == 'R') {  // local selected paper and rival selected rock
+            gameStatus = 1;
+        }
+
+        else if(localCharacter == 'S' && rivalCharacter == 'P' ) { // local selected scissor and rival selected paper
+            gameStatus = 1;
+        }
+
+        else if(localCharacter == rivalCharacter) { // Draw
+            gameStatus = 2;
+        }
+
+        else {
+            gameStatus = 0;
+        }
+    }
+
+    return gameStatus;
+}

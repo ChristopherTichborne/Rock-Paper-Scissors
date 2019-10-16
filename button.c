7defe64023ce77f88fee8698243a9c89f045e8a1
Nicholas Tice Page <ntp23@cs18207kq.canterbury.ac.nz>
2019-10-16 15:08:06 +1300

    Most likely final button module

--- button.c
+++ button.c
@@ -1,21 +1,25 @@
+/* Button.c
+ * Nic Page, Chris Tichborne
+ * 16 October 2019
+ * Module to set up button*/
+
 
 #ifndef BUTTON_H
 #define BUTTON_H
-
 
 #include <avr/io.h>
 #include "button.h"
 
 #define BIT(x) (1 << (x))
 
-/** Return non-zero if button pressed.  */
+/* Return non-zero if button pressed*/
 int button_pressed_p (void)
 {
     return (PIND & BIT(7));
 }
 
 
-/** Initialise button1.  */
+/* Initialise the button*/
 void button_init (void)
 {
     DDRD |= (0 << 7);

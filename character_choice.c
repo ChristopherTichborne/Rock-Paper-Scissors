805a63bc192a2fb5ccedeed9162d0cc2e4a0a8ea
Nicholas Tice Page <ntp23@cs18207kq.canterbury.ac.nz>
2019-10-16 15:06:58 +1300

    Most likely final character_choice.c module

--- character_choice.c
+++ character_choice.c
@@ -1,23 +1,23 @@
 /*
-    Module for selection of rock, paper, or scissors.
-    'R' for rock, 'P' for paper, and 'S' for scissors.
+ * character_choice.c
+ * Nic Page, Chris Tichborne
+ * 16 October 2019
+   Module for selection of rock, paper, or scissors.
+   'R' for rock, 'P' for paper, and 'S' for scissors.
 */
 
 #include "system.h"
 #include "navswitch.h"
 char character_choice(char choice)
 {
-
     if (navswitch_push_event_p(NAVSWITCH_EAST))    // East/Right for rock selection
     {
         choice = 'R';
     }
-
     if (navswitch_push_event_p(NAVSWITCH_NORTH))   // North/Up for paper selection
     {
         choice = 'P';
     }
-
     if (navswitch_push_event_p(NAVSWITCH_WEST))    // West/Left for scissor selection
     {
         choice = 'S';

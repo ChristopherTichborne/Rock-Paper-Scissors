c90bc1e295b1f3168620d962b586ee9190f159a5
Nicholas Tice Page <ntp23@cs18207kq.canterbury.ac.nz>
2019-10-16 13:02:20 +1300

    Header for checking winner

--- check_winner.h
+++ check_winner.h
@@ -1,6 +1,8 @@
 #ifndef CHECK_WINNER_H
 #define CHECK_WINNER_H
 
+#include "system.h"
+
 /* Sets game status flag to win, loss, or draw and returns as int */
 int check_winner(char localCharacter, char rivalCharacter);
 

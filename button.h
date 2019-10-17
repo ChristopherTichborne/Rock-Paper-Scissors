38a3dd7f46e9b0923ea12b56ae3f499a57154746
Christopher John Tichborne <cjt120@cs19114bs.canterbury.ac.nz>
2019-10-17 15:52:41 +1300

    Final commit

--- button.h
+++ button.h
@@ -5,15 +5,13 @@
  * Header file for button.c
 */
 
-
 #ifndef BUTTON_H
 #define BUTTON_H
-
 
 /* Return non-zero if button pressed.  */
 int button_pressed_p (void);
 
-
 /* Initialise the button  */
 void button_init (void);
+
 #endif

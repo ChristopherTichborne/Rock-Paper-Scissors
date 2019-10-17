95104cccbe6d0eb37ea5511996a95f0f35987a61
Nicholas Tice Page <ntp23@cs19115bs.canterbury.ac.nz>
2019-10-17 15:40:55 +1300

    Final commit

--- character_choice.c
+++ character_choice.c
@@ -9,16 +9,13 @@
 #include "navswitch.h"
 char character_choice(char choice)
 {
-    if (navswitch_push_event_p(NAVSWITCH_EAST))    // East/Right for rock selection
-    {
+    if (navswitch_push_event_p(NAVSWITCH_EAST)) {  // East/Right for rock selection
         choice = 'R';
     }
-    if (navswitch_push_event_p(NAVSWITCH_NORTH))   // North/Up for paper selection
-    {
+    if (navswitch_push_event_p(NAVSWITCH_NORTH)) { // North/Up for paper selection
         choice = 'P';
     }
-    if (navswitch_push_event_p(NAVSWITCH_WEST))    // West/Left for scissor selection
-    {
+    if (navswitch_push_event_p(NAVSWITCH_WEST)) {  // West/Left for scissor selection
         choice = 'S';
     }
 

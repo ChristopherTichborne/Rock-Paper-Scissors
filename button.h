db96f9c353de2b753497587e1b14336f0b580a39
Christopher John Tichborne <cjt120@cs19114bs.canterbury.ac.nz>
2019-10-17 14:54:23 +1300

    Updated header comments

--- button.h
+++ button.h
@@ -1,6 +1,6 @@
 /*
  * button.h
- * Nic page, Chris Tichborne
+ * Nic Page 41124128, Chris Tichborne 65767344
  * 16 October 2019
  * Header file for button.c
 */
@@ -10,10 +10,10 @@
 #define BUTTON_H
 
 
-/** Return non-zero if button pressed.  */
+/* Return non-zero if button pressed.  */
 int button_pressed_p (void);
 
 
-/** Initialise the button  */
+/* Initialise the button  */
 void button_init (void);
 #endif

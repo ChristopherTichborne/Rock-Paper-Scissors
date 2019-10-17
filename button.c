7a4b6a8a220560323b7016c129a5d83c91f52e3c
Nicholas Tice Page <ntp23@cs19115bs.canterbury.ac.nz>
2019-10-17 14:35:45 +1300

    Added usercodes

--- button.c
+++ button.c
@@ -1,8 +1,7 @@
 /* Button.c
- * Nic Page, Chris Tichborne
+ * Nic Page 41124128, Chris Tichborne 65767344
  * 16 October 2019
  * Module to set up button*/
-
 
 #ifndef BUTTON_H
 #define BUTTON_H
@@ -12,17 +11,17 @@
 
 #define BIT(x) (1 << (x))
 
-/* Return non-zero if button pressed*/
-int button_pressed_p (void)
-{
-    return (PIND & BIT(7));
-}
-
-
 /* Initialise the button*/
 void button_init (void)
 {
     DDRD |= (0 << 7);
+}
+
+
+/* Return non-zero if button pressed*/
+int button_pressed_p (void)
+{
+    return (PIND & BIT(7));
 }
 
 #endif
